1225154bc29cf827be3f5a664abbe8e909ec0681
BBrandford11 <brandfordbryce@gmail.com>
2022-01-13 23:11:05 +0000

    is even function

---
+++ even.js
@@ -0,0 +1,12 @@
+let isEeven = function(num){
+  return num % 2 === 0;
+}
+
+let tenIsEven = isEeven(10)
+let elevenIsEven = isEeven(11)
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
+console.log(isEeven(10))
+console.log(isEeven(11))
