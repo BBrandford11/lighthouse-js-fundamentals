4f028ada0fbb672920573649267819967d817857
BBrandford11 <brandfordbryce@gmail.com>
2022-01-13 01:22:23 +0000

    first commit about weather

---
+++ conditionals.js
@@ -0,0 +1,10 @@
+let temp = 12
+
+if(temp <0){
+  console.log("make sure you pick a scarf");
+}else if (temp < 15){
+  console.log("short sleeves wont cut it");
+}else {
+  console.log("Short sleeves are fine");
+}
+console.log("Now your ready to go outside");
